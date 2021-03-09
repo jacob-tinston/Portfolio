80693697b9b06e6bbf89d1b3975fd11f8c97c589
jacob-tinston <jtinston678@gmail.com>
2021-03-09 13:52:23 +0000

    added nav-2

---
+++ main.js
@@ -0,0 +1,8 @@
+let is = document.getElementById('is');
+let nav2 = document.getElementById('two');
+
+const showNav = () => {
+    nav2.style.display = 'block'
+}
+
+is.addEventListener('click', showNav)
