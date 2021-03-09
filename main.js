a827c3f67d6eaf64702f44f6f029bd8558b7227c
jacob-tinston <jtinston678@gmail.com>
2021-03-09 18:58:59 +0000

    added responsive design

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Show Home-Nav
+
 let is = document.getElementById('is');
 let nav2 = document.getElementById('two');
 
@@ -6,3 +8,24 @@ const showNav = () => {
 }
 
 is.addEventListener('click', showNav);
+
+// Show Nav (Mobile)
+
+let menu = document.getElementById('menu-icon');
+let nav = document.getElementById('nav');
+let cross = document.getElementById('cross-icon');
+
+const showNavMob = () => {
+    nav.style.display = 'flex'
+    menu.style.display = 'none'
+    cross.style.display = 'block'
+}
+
+const hideNavMob = () => {
+    nav.style.display = 'none'
+    menu.style.display = 'block'
+    cross.style.display = 'none'
+}
+
+menu.addEventListener('click', showNavMob);
+cross.addEventListener('click', hideNavMob);
